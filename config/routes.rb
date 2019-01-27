7f37203396236e781ae19ce0ee25c58593fc78f1
guohang <guohang@sudiyi.cn>
2019-01-28 03:09:13 +0800

    实现代码

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json} do
     scope module: :v1 do
-       resource :users, only: [:show]
+       resource :users, only: [:show,:create]
        end
     end
    root to: 'home#index'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) {|n| "tset#{n}@toys.com"}
+    sequence(:email) {|n| "test#{n}@toys.com"}
     password { '12345678'}
     password_confirmation { '12345678' }
   end
