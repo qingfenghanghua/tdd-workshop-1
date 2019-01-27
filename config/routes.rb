957999a447a68bd8ce5ece703648c13ad93cdea0
guohang <guohang@sudiyi.cn>
2019-01-28 01:48:47 +0800

    提交代码

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  devise_for :users
+  namespace :api, defaults: { format: :json} do
+    scope module: :v1 do
+       resource :users, only: [:show]
+       end
+    end
    root to: 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
